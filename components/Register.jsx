9697a1e5ed6890b3309c937a32d6d6f13720a1f1
Adrian Cleofe <adriancleofe@gmail.com>
2021-02-04 22:16:04 +0800

    inprogress student/teacher registration

--- components/Register.jsx
+++ components/Register.jsx
@@ -2,9 +2,52 @@ import Link from "next/link";
 import { useDispatch } from "react-redux";
 
 import { DoLogin } from "../redux/actions/UserAction";
+import {useState, useEffect} from 'react';
+import { useHttp } from "../hooks/http";
+import { useCreateAccount } from "../hooks/createAccountHook";
+
+const FieldAlert = (props) => {
+    const {message} = props;
+    return (
+    <div
+      className="w-3/4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+      role="alert"
+    >
+      <strong className="font-bold">Oops!</strong>
+      <br />
+      <span className="block sm:inline">{message}</span>
+    </div>)
+}
 
 export default function Register(props) {
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [middleName, setMiddleName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [gender, setGender] = useState('');
+
+  const [validPassword, SetValidPassword] = useState('');
+  const [disableConfirm, setDisableConfirm] = useState(true);
+
+  const [createAccount, setCreateAccount] = useState(false);
+
+  const [verifyingUsername, usernameExist] = useHttp(`http://localhost:3001/register/verify/username?value=${username}`, [username]);
+  const [verifyingEmail, emailExist] = useHttp(`http://localhost:3001/register/verify/email?value=${email}`, [email]);
+  const [creatingAccount, userData] = useCreateAccount(createAccount ? {username, email, password, firstName, middleName, lastName, mobileNumber, gender} : null)
+
   const dispatch = useDispatch();
+
+  useEffect(()=>{
+    if(!usernameExist && !emailExist && password == confirmPassword && firstName && middleName && lastName && mobileNumber && gender)
+      setDisableConfirm(false);
+    else
+      setDisableConfirm(true);
+  },[username, password,confirmPassword, email, firstName, middleName, lastName, mobileNumber, gender])
 
   return (
     <>
@@ -19,71 +62,74 @@ export default function Register(props) {
           type="text"
           className="block w-3/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="Email"
+          value={email}
+          onChange={e => { setEmail(e.target.value) }}
         />
-        <div
-          class="w-3/4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-          role="alert"
-        >
-          <strong class="font-bold">Oops!</strong>
-          <br />
-          <span class="block sm:inline">Something seriously bad happened.</span>
-        </div>
+        {/* {
+          !/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email) && <FieldAlert message="Invalid email!" />
+        } */}
+        {
+          emailExist && <FieldAlert message="Email has already been taken!" />
+        }
         <br />
         <input
           type="text"
           className="block w-3/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="Username"
+          value={username}
+          onChange={e => { setUsername(e.target.value) }}
         />
-        <div
-          class="w-3/4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-          role="alert"
-        >
-          <strong class="font-bold">Oops!</strong>
-          <br />
-          <span class="block sm:inline">Something seriously bad happened.</span>
-        </div>
+        {
+          usernameExist &&  <FieldAlert message="Username is already taken!" />
+        }
+
         <br />
         <input
           type="password"
           name="password"
           className="block w-3/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="Password"
+          value={password}
+          onChange={e => { setPassword(e.target.value) }}
         />
-        <div
-          class="w-3/4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+        {!validPassword && <div
+          className="w-3/4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
           role="alert"
         >
-          <strong class="font-bold">Oops!</strong>
-          <br />
-          <ul>
-            <li>test sdfs test</li>
-            <li>test sdfs test</li>
-            <li>test sdfs test</li>
-            <li>test sdfs test</li>
-            <li>test sdfs test</li>
-          </ul>
-        </div>
+          <strong className="font-bold">Password Strenght: {/(\w|\d|\W){8,}/.test(password) ? 'Strong' : 'Weak'}</strong>
+          { !/(a-z&A-Z|\d|\W){8,}/.test(password) && <ul className="list-disc text-sm p-2">
+            {
+              !/([A-ZÑ])+/.test(password) && <li>Password must contain atleast 1 upper case letters (A - Z)</li>
+            }
+            {
+              !/([a-zñ])+/.test(password) && <li>Password must contain atleast 1 lower case letters (a - z)</li>
+            }
+                        {
+              !/\d/.test(password) && <li>Password must contain atleast 1 number (0 - 9)</li>
+            }
+            {!/\W/.test(password) && <li>Password must contain atleast 1 non-alphanumeric symbol (e.g. '@$%@(|'))</li>}
+            {!/(\W|\d|\w){8,}/.test(password) && <li>Password must be 8 characters long</li>}
+            
+          </ul>}
+          </div>}
         <br />
         <input
           type="password"
           name="confirmPassword"
           className="block w-3/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="Re-enter your password"
+          value={confirmPassword}
+          onChange={e => {setConfirmPassword(e.target.value)}}
         />
-        <div
-          class="w-3/4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-          role="alert"
-        >
-          <strong class="font-bold">Oops!</strong>
-          <br />
-          <span class="block sm:inline">Something seriously bad happened.</span>
-        </div>
+        {password != confirmPassword && <FieldAlert message="Password does not match!" /> }
         <br />
         <input
           type="text"
           name="firstName"
           className="block w-3/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="First name"
+          value={firstName}
+          onChange={e => {setFirstName(e.target.value)}}
         />
         <br />
         <input
@@ -91,6 +137,8 @@ export default function Register(props) {
           name="middleName"
           className="block w-3/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="Middle name"
+          value={middleName}
+          onChange={e => {setMiddleName(e.target.value)}}
         />
         <br />
         <input
@@ -98,6 +146,8 @@ export default function Register(props) {
           name="lastName"
           className="block w-3/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="Last name"
+          value={lastName}
+          onChange={e => {setLastName(e.target.value)}}
         />
         <br />
         <input
@@ -105,26 +155,22 @@ export default function Register(props) {
           name="mobileNo"
           className="block w-3/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="Mobile Number"
+          value={mobileNumber}
+          onChange={e => {setMobileNumber(e.target.value)}}
+
         />
         <br />
-        <input
-          type="text"
-          name="School"
-          className="block w-3/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-          placeholder="School"
-        />
-        <br />
-        <input
-          type="text"
-          name="Grade Level"
-          className="block w-3/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-          placeholder="Grade Level"
-        />
+        <h6 className="text-xl font-bold text-gray-700">Gender:</h6>
+        <div onChange={e => setGender(e.target.value)}>
+        <input type="radio" name="gender" value="male" /> Male &nbsp; &nbsp;
+        <input type="radio" name="gender" value="female" /> Female
+        </div>
         <br />
         <div className="flex space-x-4">
           <button
-            onClick={(e) => dispatch(DoLogin(true, { name: "test" }))}
-            className="bg-green-600 text-white text-xl font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-green-200"
+            onClick={(e) => setCreateAccount(true)}
+            className={`${disableConfirm ? "opacity-50 cursor-not-allowed": "hover:bg-green-700" } bg-green-600 text-white text-xl font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-green-200`}
+            disabled={disableConfirm}
           >
             Confirm
           </button>
@@ -132,7 +178,7 @@ export default function Register(props) {
             onClick={(e) => props.changeStatus("login")}
             className="bg-blue-500 text-white text-xl font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 focus:ring-offset-blue-300"
           >
-            Register
+            Login
           </button>
         </div>
       </div>
