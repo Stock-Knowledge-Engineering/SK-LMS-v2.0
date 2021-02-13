5d08f31bb5cb68459139c1e41c9d91c267a1a106
Adrian Cleofe <adriancleofe@gmail.com>
2021-02-13 20:20:08 +0800

    Room File Maintenace

---
+++ pages/admin/rooms.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Start from "../../components/Start";
+import { useSelector } from "react-redux";
+
+import SchoolAdminLayout from "../../layouts/SchoolAdminLayout";
+import { useUserManagementHook } from "../../hooks/userManagementHook";
+import SubjectAddModal from "../../components/SubjectAddModal";
+import RoomTable from "../../components/RoomTable";
+import GradeLevelEditModal from "../../components/GradeLevelEditModal";
+import RoomAddModal from '../../components/RoomAddModal';
+import RoomEditModal from "../../components/RoomEditModal";
+
+export default function Index() {
+  const [status, setStatus] = useState("admin-login");
+  const [isAddModalOpen, showAddModal] = useState(false);
+  const [isEditModalOpen, showEditModal] = useState(false);
+  const [room, setGradeLevel] = useState(null);
+
+  const user = useSelector((state) => state.UserReducer);
+
+  useUserManagementHook();
+
+  function editData (room){
+    setGradeLevel(room);
+      showEditModal(true);
+  }
+
+  return (
+    <>
+      {!user.isLogin ? (
+        <Start status={status} changeStatus={setStatus} />
+      ) : (
+        <SchoolAdminLayout>
+          <br />
+          <div className="flex flex-col relative ">
+            <div className="text-right">
+              <button className="m-auto h-12 w-12 bg-green-300 rounded-full text-center text-white text-3xl" onClick={ e => showAddModal(true)}>
+                &#43;
+              </button>
+            </div>
+            <RoomTable editData={editData} isAddModalOpen = {isAddModalOpen} isEditModalOpen = {isEditModalOpen}/>
+            {isAddModalOpen && <RoomAddModal schoolid={user.data.school_id} showAddModal={showAddModal}/>}
+            {isEditModalOpen && <RoomEditModal room={room} showEditModal={showEditModal}/>}
+          </div>
+        </SchoolAdminLayout>
+      )}
+    </>
+  );
+}
