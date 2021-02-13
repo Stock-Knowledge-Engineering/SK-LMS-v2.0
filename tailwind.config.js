df47d4b328dbe8a2f0d6095a8ebe4c9892923968
Adrian Cleofe <adriancleofe@gmail.com>
2021-02-13 20:22:21 +0800

    added left and bottom option

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
   // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    left: {
+      '1': '0.1em',
+      '2': '0.2em'
+    },
+    bottom:{
+      '1': '0.1',
+      '2': '0.2',
+      '3': '0.3',
+      '4': '0.4'
+    },
     extend: {},
   },
   variants: {
