635efb1abeb635163491280e9827cd6ffc72ea7a
Adrian Cleofe <adriancleofe@gmail.com>
2021-02-19 21:43:05 +0800

    fix layout

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -4,17 +4,18 @@ import Menu from '../components/Menu';
 export default function MainLayout(props){
     return(
         <>
-            <div id="dashboard">
-                <div id="dash-menu">
+            <div id="dashboard" className="flex">
+                <div id="dash-menu" className="text-white w-1/5 h-screen">
                     <MiniProfile />
                     <div className="clear-both"></div>
                     <Menu />
                 </div>
-                <div id="dash-content">
-                    <div id="dash-home" className="block">
+                <div id="dash-content" className="p-4 w-4/5">
+                    <div id="dash-home">
                         <div id="search" className="text-right">
                             <input type="text" placeholder="Quick search..." />
                         </div>
+                        {props.children}
                     </div>
                 </div>
             </div>
