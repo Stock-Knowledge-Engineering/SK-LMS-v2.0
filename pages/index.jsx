bff2a9edab91659b3689e3360382217d6fc1bca0
Adrian Cleofe <adriancleofe@gmail.com>
2021-02-19 21:43:30 +0800

    added extra security

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import {useSelector} from 'react-redux'
 import MainLayout from '../layouts/MainLayout';
@@ -7,12 +7,19 @@ import MainLayout from '../layouts/MainLayout';
 import Start from '../components/Start';
 
 import {useUserManagementHook} from '../hooks/userManagementHook';
+import { useRouter } from 'next/dist/client/router';
 
 export default function Home() {
   useUserManagementHook();
 
   const [status, setStatus] = useState('login');
   const user = useSelector(state => state.UserReducer);
+  const router = useRouter();
+
+  useEffect(() => {
+    if(user.isLogin && user.data.title.toLowerCase() == 'school-admin')
+      router.push("/admin");
+  },[user.isLogin])
 
   return (
     <>
@@ -20,7 +27,7 @@ export default function Home() {
       !user.isLogin && <Start status={status} changeStatus={setStatus}/>
     }
     {
-      user.isLogin && user.data.title.toLowerCase() == 'student' && <MainLayout>
+      user.isLogin && user.data.title.toLowerCase() != 'school-admin' && <MainLayout>
         <h1>Dashboard is in development!</h1>
       </MainLayout>
     }
