1251d3d7010aeeae7b736e44133c304670b5762b
Adrian Cleofe <adriancleofe@gmail.com>
2021-02-27 21:57:29 +0800

    Added School Registration Page

---
+++ pages/admin/registration.jsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import { useEffect, useState } from 'react';
+
+import {useSelector} from 'react-redux'
+import MainLayout from '../../layouts/MainLayout';
+
+import Start from '../../components/Start';
+
+import {useUserManagementHook} from '../../hooks/userManagementHook';
+import { useRouter } from 'next/dist/client/router';
+
+export default function Code(props) {
+
+  useUserManagementHook();
+  const [status, setStatus] = useState('school-registration');
+  const user = useSelector(state => state.UserReducer);
+  const router = useRouter();
+
+  useEffect(() => {
+    if(user.data && user.data.title.toLowerCase() == 'teacher')
+      router.push("/");
+    if(user.data && user.data.title.toLowerCase() == 'school-admin')
+      router.push("/school");
+    
+  },[user])
+
+  return (
+    <>
+    {
+      !user.isLogin && <Start code={router.query.code} page="/admin/registration" status={status} changeStatus={setStatus}/>
+    }
+    {
+      user.isLogin && <MainLayout>
+        <h1>Dashboard is in development!</h1>
+      </MainLayout>
+    }
+    </>
+  )
+}
