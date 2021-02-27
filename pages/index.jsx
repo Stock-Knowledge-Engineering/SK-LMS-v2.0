ed4b710078d59e836bdb3b00d23ca8b37e43757e
Adrian Cleofe <adriancleofe@gmail.com>
2021-02-27 22:05:13 +0800

    Fix bugs

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ import Start from '../components/Start';
 import {useUserManagementHook} from '../hooks/userManagementHook';
 import { useRouter } from 'next/dist/client/router';
 
-export default function Home() {
+export default function Home(props) {
   useUserManagementHook();
 
   const [status, setStatus] = useState('login');
@@ -17,17 +17,21 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    if(user.isLogin && user.data.title.toLowerCase() == 'school-admin')
-      router.push("/admin");
+    if(!user.data)
+      router.push("/login");
+
+    if(user.isLogin){
+      router.push("/")
+    }
   },[user.isLogin])
 
   return (
     <>
     {
-      !user.isLogin && <Start status={status} changeStatus={setStatus}/>
+      !user.isLogin && <Start page={props.page} status={status} changeStatus={setStatus}/>
     }
     {
-      user.isLogin && user.data.title.toLowerCase() != 'school-admin' && <MainLayout>
+      user.isLogin && <MainLayout>
         <h1>Dashboard is in development!</h1>
       </MainLayout>
     }
