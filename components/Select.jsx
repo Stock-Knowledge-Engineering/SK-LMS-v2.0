1cbaced4c8e05ba268dfcb258b7d21b93c322c39
Adrian Cleofe <adriancleofe@gmail.com>
2021-03-10 20:20:55 +0800

    Update Select Component

--- components/Select.jsx
+++ components/Select.jsx
@@ -1,12 +1,24 @@
-export default function Select (props) {
+const Select = (props) => {
     return (
-        <select defaultValue={props.selected}>
-            <option value=""></option>
-            {props.options && props.options.map(option => {
-                return (
-                    <option selected={option[props.label]==props.selected} key={option.id} value={option.id}>{option[props.label]}</option>
-                )
-            })}
-        </select>
+      <select
+        defaultValue={props.selected}
+        onChange={(e) => props.setValue(e.target.value)}
+      >
+        <option value="">------{props.placeholder}------</option>
+        {props.options &&
+          props.options.map((option, index) => {
+            return (
+              <option
+                selected={option[props.label] == props.selected}
+                key={index}
+                value={option[props.value]}
+              >
+                {option[props.label]}
+              </option>
+            );
+          })}
+      </select>
     );
-}
+  };
+
+  export default Select;
