0bd931e466ff269a9694c922fbe4c3271c6658ec
Adrian Cleofe <adriancleofe@gmail.com>
2021-03-28 10:55:20 +0800

    Fix MobileNavBar

--- components/MobileNavBar.jsx
+++ components/MobileNavBar.jsx
@@ -2,7 +2,7 @@ import {useRef, useState, useEffect, useCallback} from 'react';
 
 const Menu = (props) => {
     return (
-        <ul onClick={e => {props.showMenu(false)}} className="hero top-15 py-8 h-screen w-height text-center text-4xl text-white uppercase font-bold space-y-6">
+        <ul onClick={e => {props.showMenu(false)}} className="hero top-15 py-8 h-screen w-screen text-center text-4xl text-white uppercase font-bold space-y-6">
             <li>
                 <a href="#home">Home</a>
             </li>
@@ -48,13 +48,13 @@ const MobileNavbar = (props) => {
     },[])
 
     return (
-        <div ref={ref} className={`${float ? 'fixed' : ''} hero flex flex-col items-center md:hidden xs:block xs:w-height z-50`}>
+        <div ref={ref} className={`${float ? 'fixed' : ''} bg-white flex flex-col items-center md:hidden xs:block xs:w-screen z-50`}>
             <div className="w-full px-2 py-4 flex text-white justify-between">
-                <a href="#home" className="flex items-center text-xl text-white">
-                    <img src="/images/logo-white.svg" />
-                    <h4 className="lg:flex md:hidden text-4xl">Stock <span className="font-bold">Knowledge</span></h4>
+                <a href="#home" className="flex items-center text-2xl text-white space-x-2">
+                    <img className="w-14" src="/images/logo.png" />
+                    <h4 className="lg:flex md:hidden text-xl text-heading">Stock <span className="font-bold text-skBlue">Knowledge</span></h4>
                 </a >
-                <svg onClick={(e) => setDisplayMenu(!displayMenu)} className="w-16 h-16 border p-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg onClick={(e) => setDisplayMenu(!displayMenu)} className="w-16 h-16 text-skBlue p-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
             </div>
