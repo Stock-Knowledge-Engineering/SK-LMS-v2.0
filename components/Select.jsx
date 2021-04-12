4cc0bff4be580b1b81e9018820376de06d4d667e
Adrian Cleofe <adriancleofe@gmail.com>
2021-04-13 01:15:58 +0800

    Revise select component

--- components/Select.jsx
+++ components/Select.jsx
@@ -1,19 +1,20 @@
-const Select = (props) => {
+const Select = ({classNames, selected, setValue, placeholder, label, compare, value, options}) => {
     return (
       <select
-        defaultValue={props.selected}
-        onChange={(e) => props.setValue(e.target.value)}
+        className={classNames}
+        defaultValue={selected}
+        onChange={(e) => setValue(e.target.value)}
       >
-        <option value="">------{props.placeholder}------</option>
-        {props.options &&
-          props.options.map((option, index) => {
+        <option value="">------{placeholder}------</option>
+        {options &&
+          options.map((option, index) => {
             return (
               <option
-                selected={option[props.label] == props.selected}
+                selected={option[compare] == selected}
                 key={index}
-                value={option[props.value]}
+                value={option[value]}
               >
-                {option[props.label]}
+                {option[label]}
               </option>
             );
           })}
