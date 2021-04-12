ec02b179dc90e6357d0a4f3fac78ef03a8624303
Adrian Cleofe <adriancleofe@gmail.com>
2021-04-13 01:29:04 +0800

    InterestComponents

---
+++ components/HomePage/InterestComponents.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import TextArea from "../TextArea";
+import TextField from "../TextField";
+
+const InterestComponents = ({
+  favoriteSubject,
+  setFavoriteSubject,
+  careerGoal,
+  setCareerGoal,
+  setStep,
+}) => {
+  const [disable, setDisable] = useState(true);
+
+  useEffect(()=>{
+    if(favoriteSubject && favoriteSubject.match(/^[A-Za-z ]*/)[0] == favoriteSubject && careerGoal && careerGoal.match(/^[A-Za-z ]*/)[0] == careerGoal)
+      setDisable(false);
+    else
+      setDisable(true);
+  },[favoriteSubject, careerGoal])
+
+  return (
+    <>
+      <TextField
+        classNames="lg:w-full md:w-full sm:w-full xs:w-3/4 mt-2 rounded-xl border border-lightGray placeholder-lightGray"
+        placeholder="What is your favorite Subject?"
+        value={favoriteSubject}
+        setValue={setFavoriteSubject}
+        alert={true}
+      />
+      <TextArea
+        value={careerGoal}
+        setValue={setCareerGoal}
+        classNames="lg:w-full md:w-full sm:w-full xs:w-3/4 mt-2 rounded-xl border border-lightGray placeholder-lightGray"
+        setValue={setCareerGoal}
+        value={careerGoal}
+        placeholder="Academic or Career Goal"
+      />
+      <button
+        disabled={disable ? true : false}
+        onClick={() => {
+          setStep(3);
+        }}
+        className="bg-blue-500 text-white text-xl font-semibold w-full mt-10 py-3 rounded-full disabled:opacity-50"
+      >
+        Next
+      </button>
+    </>
+  );
+};
+
+export default InterestComponents;
