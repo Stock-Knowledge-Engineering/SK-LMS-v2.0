4ea8b572237f49d9073832703609efa334c1355e
Adrian Cleofe <adriancleofe@gmail.com>
2021-04-13 01:17:42 +0800

    Revise ConfirmPasswordInput

---
+++ components/ConfirmPasswordInput.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+import FieldAlert from "./FieldAlert";
+
+export default function ConfirmPasswordInput({
+  wrapperClassName,
+  textFieldClassName,
+  placeholder,
+  value,
+  setValue,
+  password,
+}) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const revealEye = (
+    <FontAwesomeIcon
+      onClick={() => {
+        setShowPassword(!showPassword);
+      }}
+      icon={faEye}
+      size="lg"
+      color="lightGray"
+      className="absolute right-4 top-3 cursor-pointer hover:text-subheading"
+    />
+  );
+
+  const hideEye = (
+    <FontAwesomeIcon
+      onClick={() => {
+        setShowPassword(!showPassword);
+      }}
+      icon={faEyeSlash}
+      size="lg"
+      color="lightGray"
+      className="absolute right-4 top-3 cursor-pointer hover:text-subheading"
+    />
+  );
+
+  return (
+    <>
+      <div className={`${wrapperClassName} relative`}>
+        <input
+          type={showPassword ? "text" : "password"}
+          name="confirmPassword"
+          className={`w-full ${textFieldClassName}`}
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        {!showPassword ? revealEye : hideEye}
+      </div>
+
+      {password != value && (
+        <FieldAlert
+          classNames="w-full mt-2 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          message="Password does not match!"
+        />
+      )}
+    </>
+  );
+}
