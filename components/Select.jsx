77a4f8bb6699fd36752c57b06a63383dc5bba5f1
Adrian Cleofe <adriancleofe@gmail.com>
2021-05-03 02:08:07 +0800

    cleanup

--- components/Select.jsx
+++ components/Select.jsx
@@ -5,7 +5,7 @@ const Select = ({classNames, selected, setValue, placeholder, label, compare, va
         defaultValue={selected}
         onChange={(e) => setValue(e.target.value)}
       >
-        <option value="">------{placeholder}------</option>
+        <option value="">------ {placeholder} ------</option>
         {options &&
           options.map((option, index) => {
             return (
