9ecaeb93976c577cc2e2629a9cff6fd78ee12de2
Adrian Cleofe <adriancleofe@gmail.com>
2021-05-24 20:32:53 +0800

    fix menu

--- components/LMS/Menu/Menu.jsx
+++ components/LMS/Menu/Menu.jsx
@@ -1,50 +1,96 @@
 import { faBookmark, faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
 import { faHome, faTrophy } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/dist/client/router";
+import { useSelector } from "react-redux";
 
 const Menu = (props) => {
-    const homeIcon = (
-        <FontAwesomeIcon
-          icon={faHome}
-          size="lg"
-          color=""
-          className={`text-skBlue cursor-pointer hover:text-subheading`}
-        />
-      );
-    
-      const bookmarkIcon = (
-        <FontAwesomeIcon
-          icon={faBookmark}
-          size="lg"
-          color="lightGray"
-          className={`cursor-pointer hover:text-subheading`}
-        />
-      );
-    
-      const calendarIcon = (
-        <FontAwesomeIcon
-          icon={faCalendarAlt}
-          size="lg"
-          color="lightGray"
-          className={`cursor-pointer hover:text-subheading`}
-        />
-      );
-    
-      const leaderboardIcon = (
-        <FontAwesomeIcon
-          icon={faTrophy}
-          size="lg"
-          color="lightGray"
-          className={`cursor-pointer hover:text-subheading`}
-        />
-      );
+  const router = useRouter();
+  const user = useSelector((state) => state.UserReducer);
+
+  const { slug } = router.query;
+
+  const homeIcon = (
+    <FontAwesomeIcon
+      icon={faHome}
+      size="lg"
+      color=""
+      className={`${
+        !slug ||
+        (slug &&
+          slug[0] != user.data.username &&
+          slug &&
+          slug[0] != "my-schedule" &&
+          slug[0] != "courses")
+          ? "text-skBlue"
+          : "text-lightGray hover:text-skBlue"
+      } cursor-pointer `}
+    />
+  );
+
+  const bookmarkIcon = (
+    <FontAwesomeIcon
+      icon={faBookmark}
+      size="lg"
+      color="lightGray"
+      className={`${
+        slug && slug[0] == "courses"
+          ? "text-skBlue"
+          : "text-lightGray hover:text-skBlue"
+      } cursor-pointer`}
+    />
+  );
+
+  const calendarIcon = (
+    <FontAwesomeIcon
+      icon={faCalendarAlt}
+      size="lg"
+      color="lightGray"
+      className={`cursor-pointer hover:text-subheading`}
+    />
+  );
+
+  const leaderboardIcon = (
+    <FontAwesomeIcon
+      icon={faTrophy}
+      size="lg"
+      color="lightGray"
+      className={`cursor-pointer hover:text-subheading`}
+    />
+  );
   return (
     <>
-      <div className="flex items-center w-full h-16 pl-10 bg-blue-50 rounded-l-full border-skBlue space-x-4 border-r-4">
+      <div
+        onClick={() => {
+          router.push("/lms/", undefined, { scroll: true, shallow: false });
+        }}
+        className={`flex items-center w-full h-16 pl-10 space-x-4 ${
+          !slug ||
+          (slug &&
+            slug[0] != user.data.username &&
+            slug &&
+            slug[0] != "my-schedule" &&
+            slug[0] != "courses")
+            ? "bg-blue-50 rounded-l-full border-skBlue border-r-4 text-skBlue"
+            : "text-lightGray"
+        }`}
+      >
         {homeIcon}
-        <p className="text-skBlue">Home</p>
+        <p>Home</p>
       </div>
-      <div className="flex items-center w-full h-16 pl-10 space-x-4">
+      <div
+        onClick={() => {
+          router.push("/lms/courses/", undefined, {
+            scroll: false,
+            shallow: true,
+          });
+        }}
+        className={`flex items-center w-full h-16 pl-10 space-x-4 ${
+          slug && slug[0] == "courses"
+            ? "bg-blue-50 rounded-l-full border-skBlue border-r-4 text-skBlue"
+            : "text-lightGray"
+        }`}
+      >
         {bookmarkIcon}
         <p className="text-lightGray">Courses</p>
       </div>
@@ -59,6 +105,5 @@ const Menu = (props) => {
     </>
   );
 };
-
 
 export default Menu;
--- components/LMS/Menu/MobileMenu.jsx
+++ components/LMS/Menu/MobileMenu.jsx
@@ -3,16 +3,45 @@ import {
   faHome,
   faSignOutAlt,
   faTrophy,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/dist/client/router";
+import { useDispatch, useSelector } from "react-redux";
+import { DoLogin } from "../../../redux/actions/UserAction";
 
 const MobileMenu = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const user = useSelector((state) => state.UserReducer);
+
+  const { slug } = router.query;
+
   const homeIcon = (
     <FontAwesomeIcon
       icon={faHome}
       size="lg"
       color=""
-      className={`text-white cursor-pointer hover:text-subheading`}
+      className={`${
+        !slug ||
+        (slug &&
+          slug[0] != user.data.username &&
+          slug &&
+          slug[0] != "my-schedule")
+          ? "bg-skBlue text-white"
+          : ""
+      } cursor-pointer hover:text-subheading`}
+    />
+  );
+
+  const userIcon = (
+    <FontAwesomeIcon
+      icon={faUser}
+      size="lg"
+      color=""
+      className={`${
+        slug && slug[0] == user.data.username ? "text-white" : "text-skBlue"
+      } cursor-pointer hover:text-subheading`}
     />
   );
 
@@ -30,7 +59,9 @@ const MobileMenu = () => {
       icon={faCalendarAlt}
       size="lg"
       color="lightGray"
-      className={`text-skBlue cursor-pointer hover:text-subheading`}
+      className={`${
+        slug && slug[0] == "my-schedule" ? "text-white" : "text-skBlue"
+      } cursor-pointer hover:text-subheading`}
     />
   );
 
@@ -51,18 +82,55 @@ const MobileMenu = () => {
       className={`text-skBlue cursor-pointer hover:text-subheading`}
     />
   );
+
+  const Logout = () => {
+    dispatch(DoLogin(false, null));
+    router.push("/lms", undefined, { shallow: true });
+  };
+
   return (
     <div className="box-border h-screen w-full bg-blue-50 flex flex-col py-2">
       <div className="box-border border w-full h-auto bg-white text-skBlue">
-        <div className="bg-skBlue text-white box-border h-auto p-4 flex justify-start items-center space-x-2 border-b">
+        <div
+          onClick={() => {
+            router.push("/lms");
+          }}
+          className={`${
+            !slug ||
+            (slug &&
+              slug[0] != user.data.username &&
+              slug &&
+              slug[0] != "my-schedule")
+              ? "bg-skBlue text-white"
+              : ""
+          } box-border h-auto p-4 flex justify-start items-center space-x-2 border-b cursor-pointer`}
+        >
           {homeIcon}
           <h4>Home</h4>
+        </div>
+        <div
+          onClick={() => {
+            router.push(`/lms/${user.data.username}`);
+          }}
+          className={`${
+            slug && slug[0] == user.data.username ? "bg-skBlue text-white" : ""
+          } box-border h-auto p-4 flex justify-start items-center space-x-2 border-b cursor-pointer`}
+        >
+          {userIcon}
+          <h4>Profile</h4>
         </div>
         <div className="box-border h-auto p-4 flex justify-start items-center space-x-2 border-b">
           {bookmarkIcon}
           <h4>Courses</h4>
         </div>
-        <div className="box-border h-auto p-4 flex justify-start items-center space-x-2 border-b">
+        <div
+          onClick={() => {
+            router.push(`/lms/my-schedule`);
+          }}
+          className={`${
+            slug && slug[0] == "my-schedule" ? "bg-skBlue text-white" : ""
+          } box-border h-auto p-4 flex justify-start items-center space-x-2 border-b cursor-pointer`}
+        >
           {calendarIcon}
           <h4>My Schedule</h4>
         </div>
@@ -70,7 +138,12 @@ const MobileMenu = () => {
           {leaderboardIcon}
           <h4>Home</h4>
         </div>
-        <div className="box-border h-auto p-4 flex justify-start items-center space-x-2 border-b">
+        <div
+          onClick={() => {
+            Logout();
+          }}
+          className="box-border h-auto p-4 flex justify-start items-center space-x-2 border-b cursor-pointer"
+        >
           {logoutIcon}
           <h4>Logout</h4>
         </div>
