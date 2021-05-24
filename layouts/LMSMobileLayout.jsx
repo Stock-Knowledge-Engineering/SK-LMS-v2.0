eb594033646fc45ce67b1eebc880260da4f8b6ad
Adrian Cleofe <adriancleofe@gmail.com>
2021-05-24 20:32:32 +0800

    cleanup

--- layouts/LMSMobileLayout.jsx
+++ layouts/LMSMobileLayout.jsx
@@ -1,25 +1,20 @@
-import { faBookmark, faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
-import {
-  faAngleDown,
-  faBars,
-  faBell,
-  faChartPie,
-  faCheckDouble,
-  faCog,
-  faHome,
-  faPlay,
-  faQuestionCircle,
-  faSearch,
-  faSignOutAlt,
-  faTrophy,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBars, faPlay, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/dist/client/router";
+import { useSelector } from "react-redux";
 import CalendarLayout from "../components/Calendar/CalendarLayout";
 import MobileMenu from "../components/LMS/Menu/MobileMenu";
+import MiniProfile from "../components/LMS/MobileSecondaryMenu";
+import ModuleActivityWrapper from "../components/LMS/ModuleActivity/ModuleActivityWrapper";
+import Dashboard from "../components/LMS/Dashboard";
+import Profile from "../components/LMS/Profile";
+import Settings from "../components/LMS/Settings";
+import MySchedule from "../components/LMS/MySchedule";
 
 const LMSMobileLayout = (props) => {
+  const user = useSelector(state => state.UserReducer);
   const router = useRouter();
+  const { slug } = router.query;
 
   const menuIcon = (
     <FontAwesomeIcon
@@ -28,8 +23,12 @@ const LMSMobileLayout = (props) => {
       className={`text-skBlue cursor-pointer`}
       onClick={() => {
         router.query.menu
-          ? router.push("/lms", undefined, { shallow: true })
-          : router.push("?menu=true", undefined, { shallow: true });
+          ? router.push(`${window.location.pathname}`, undefined, {
+              shallow: true,
+            })
+          : router.push(`${window.location.pathname}?menu=true`, undefined, {
+              shallow: true,
+            });
       }}
     />
   );
@@ -39,54 +38,6 @@ const LMSMobileLayout = (props) => {
       icon={faSearch}
       size="lg"
       className={`text-skBlue cursor-pointer`}
-    />
-  );
-
-  const questionMarkIcon = (
-    <FontAwesomeIcon
-      icon={faQuestionCircle}
-      size="lg"
-      className={`text-skBlue cursor-pointer`}
-    />
-  );
-
-  const gearIcon = (
-    <FontAwesomeIcon
-      icon={faCog}
-      size="lg"
-      className={`text-skBlue cursor-pointer`}
-    />
-  );
-
-  const bellIcon = (
-    <FontAwesomeIcon
-      icon={faBell}
-      size="lg"
-      className={`text-skBlue cursor-pointer`}
-    />
-  );
-
-  const pieChartIcon = (
-    <FontAwesomeIcon
-      icon={faChartPie}
-      size="2x"
-      className={`text-skBlue cursor-pointer hover:text-subheading m-auto`}
-    />
-  );
-
-  const availableCoursesIcon = (
-    <FontAwesomeIcon
-      icon={faBookmark}
-      size="2x"
-      className={`text-skBlue cursor-pointer hover:text-subheading m-auto`}
-    />
-  );
-
-  const completedCoursesIcon = (
-    <FontAwesomeIcon
-      icon={faCheckDouble}
-      size="2x"
-      className={`text-skBlue cursor-pointer hover:text-subheading m-auto`}
     />
   );
 
@@ -104,98 +55,21 @@ const LMSMobileLayout = (props) => {
         <div className="w-full p-4 mb-1 box-border flex flex-row items-center justify-between bg-white">
           {menuIcon}
           <div className="flex space-x-2">
-            <img className="w-8" src="images/logo.png" />
+            <img className="w-8" src="/images/logo.png" />
             <h1 className="text-subheading">
               Stock <span className="text-skBlue font-semibold">Knowledge</span>
             </h1>
           </div>
           {searchIcon}
         </div>
-        <div className="w-full p-4 flex flex-row items-center justify-between bg-white">
-          <div className="box-border w-auto flex flex-row space-x-2">
-            <img
-              className="h-12 rounded-full border"
-              src="/images/avatar.png"
-            />
-            <div className="box-border flex flex-col">
-              <p className="text-heading font-semibold truncate">
-                Firstname Lastname
-              </p>
-              <p className="text-subheading">ID: 000000</p>
-            </div>
-          </div>
-          <div className="w-auto box-border flex flex-row space-x-4">
-            {questionMarkIcon}
-            {gearIcon}
-            {bellIcon}
-          </div>
-        </div>
+        <MiniProfile />
       </div>
       {router.query.menu && <MobileMenu />}
-      <div className="box-border h-screen w-full bg-blue-50 flex flex-col py-2 space-y-2">
-        <div className="w-full h-auto box-border relative px-4">
-          <img
-            className="w-full h-full"
-            src="/images/dashboard-content-1.svg"
-          />
-          <p className="absolute top-2 left-8 text-skBlue font-bold">
-            Hi Firstname,
-          </p>
-        </div>
-        <div className="w-full h-auto flex flex-col justify-around relative px-4 space-y-2">
-          <div className="h-full w-full p-2 flex bg-white border rounded-2xl">
-            <div className="w-1/6 h-full flex items-center">{pieChartIcon}</div>
-            <div className="flex flex-col justify-center h-full w-3/4">
-              <p className="text-skBlue font-bold text-xl">20</p>
-              <p className="font-semibold text-subheading">Ongoing Courses</p>
-            </div>
-          </div>
-          <div className="h-full w-full p-2 flex bg-white border rounded-2xl">
-            <div className="w-1/6 h-full flex items-center">
-              {availableCoursesIcon}
-            </div>
-            <div className="flex flex-col justify-center h-full w-3/4">
-              <p className="text-skBlue font-bold text-xl">6</p>
-              <p className="font-semibold text-subheading">Available Courses</p>
-            </div>
-          </div>
-          <div className="h-full w-full p-2 flex bg-white border rounded-2xl">
-            <div className="w-1/6 h-full flex items-center">
-              {completedCoursesIcon}
-            </div>
-            <div className="flex flex-col justify-center h-full w-3/4">
-              <p className="text-skBlue font-bold text-xl">1</p>
-              <p className="font-semibold text-subheading">Completed Courses</p>
-            </div>
-          </div>
-        </div>
-        <div className="box-border h-auto w-full flex flex-col py-2 px-4 rounded-2xl">
-          <CalendarLayout />
-        </div>
-        <div className="box-border h-auto w-full flex flex-col bg-blue-50 py-2 px-4 rounded-2xl space-y-2">
-          <h1 className="text-heading text-lg font-semibold ">Module Activity</h1>
-          <div className="w-full bg-skBlue rounded-2xl text-white box-border flex flex-col items-end justify-between p-4 space-y-2">
-            <div className="box-border w-full flex justify-between">
-              <div className="relative border-b-2 border-white w-10">
-                <h5 className="absolute font-semibold text-xl">Module</h5>
-              </div>
-              <h4 className="text-4xl text-white font-bold">01</h4>
-            </div>
-            <div className="h-2 w-full bg-skBlueInactive rounded-full">
-              <div
-                style={{ width: "44%" }}
-                className="h-full bg-white rounded-full"
-              ></div>
-            </div>
-            <div className="box-border w-full h-auto flex flex-row items-center justify-between">
-              {modulePlayIcon}
-              <div className="box-border flex flex-row items-center justify-between">
-                <h5 className="font-bold">44% <span className="font-normal">Completed</span></h5>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {!slug && !router.query.menu && <Dashboard />}
+      {(slug && slug[0] != user.data.username) && !router.query.menu && slug[0] != 'settings' && !router.query.menu && slug[0] != 'my-schedule' && <Dashboard />}
+      {slug && slug[0] == user.data.username && !router.query.menu && <Profile />}
+      {slug && slug[0] == 'settings' && <Settings />}
+      {slug && slug[0] == 'my-schedule' && <MySchedule />}
     </>
   );
 };
--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -1,55 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faSearch,
-  faQuestionCircle,
-  faCog,
-  faBell,
-  faAngleDown,
-  faHome,
-  faTrophy,
-  faChartPie,
-  faCheckDouble,
-  faAngleLeft,
-  faAngleRight,
-  faCircle,
-} from "@fortawesome/free-solid-svg-icons";
-import { faBookmark, faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import CalendarLayout from "../components/Calendar/CalendarLayout";
 import Menu from "../components/LMS/Menu/Menu";
+import Home from "../components/LMS/Home/Home";
+import SecondaryMenu from "../components/NavBar/SecondaryMenu";
+import Search from "../components/NavBar/Search/Search";
+import BrandLogo from "../components/NavBar/BrandLogo/BrandLogo";
+import { useRouter } from "next/dist/client/router";
+import { useSelector } from "react-redux";
+import Courses from "../components/LMS/Courses/Courses";
+import Course from "../components/LMS/Course/Course";
+import CourseTopic from "../components/LMS/CourseTopic/CourseTopic";
 
 export default function MainLayout(props) {
-  const searchIcon = (
-    <FontAwesomeIcon
-      icon={faSearch}
-      size="lg"
-      color="lightGray"
-      className={`absolute left-4 top-3 bottom-0 cursor-pointer hover:text-subheading`}
-    />
-  );
+  const router = useRouter();
+  const user = useSelector((state) => state.UserReducer);
 
-  const questionMarkIcon = (
-    <FontAwesomeIcon
-      icon={faQuestionCircle}
-      size="lg"
-      className={`text-skBlue cursor-pointer`}
-    />
-  );
-
-  const gearIcon = (
-    <FontAwesomeIcon
-      icon={faCog}
-      size="lg"
-      className={`text-skBlue cursor-pointer`}
-    />
-  );
-
-  const bellIcon = (
-    <FontAwesomeIcon
-      icon={faBell}
-      size="lg"
-      className={`text-skBlue cursor-pointer`}
-    />
-  );
+  const { slug } = router.query;
 
   const angleDownIcon = (
     <FontAwesomeIcon
@@ -59,100 +26,12 @@ export default function MainLayout(props) {
       className={`cursor-pointer hover:text-subheading`}
     />
   );
-
-  const pieChartIcon = (
-    <FontAwesomeIcon
-      icon={faChartPie}
-      size="2x"
-      color="lightGray"
-      className={`cursor-pointer hover:text-subheading m-auto`}
-    />
-  );
-
-  const availableCoursesIcon = (
-    <FontAwesomeIcon
-      icon={faBookmark}
-      size="2x"
-      color="lightGray"
-      className={`cursor-pointer hover:text-subheading m-auto`}
-    />
-  );
-
-  const completedCoursesIcon = (
-    <FontAwesomeIcon
-      icon={faCheckDouble}
-      size="2x"
-      color="lightGray"
-      className={`cursor-pointer hover:text-subheading m-auto`}
-    />
-  );
-
-  const calendarBackIcon = (
-    <FontAwesomeIcon
-      icon={faAngleLeft}
-      size="2x"
-      color=""
-      className={`text-skBlue cursor-pointer hover:text-subheading`}
-    />
-  );
-
-  const calendarForwardIcon = (
-    <FontAwesomeIcon
-      icon={faAngleRight}
-      size="2x"
-      color=""
-      className={`text-skBlue cursor-pointer hover:text-subheading`}
-    />
-  );
-
-  const calendarMarkIcon = (
-    <FontAwesomeIcon
-      icon={faCircle}
-      className={`w-2 h-2 text-skBlue absolute bottom-0`}
-    />
-  );
-
   return (
     <>
       <div className="relative">
-        <div className="fixed flex items-center h-20 w-96 py-4 px-8 text-xl space-x-2">
-          <img className="w-12" src="/images/logo.png" />
-          <h1 className="text-heading">
-            Stock &nbsp;
-            <span className="font-bold text-skBlue">Knowledge</span>
-          </h1>
-        </div>
-        <div className="fixed w-full top-0 right-0 left-0 h-20 flex items-center justify-center shadow">
-          <div className="h-full w-1/4 py-5">
-            <div className="flex w-full relative">
-              {searchIcon}
-              <input
-                className="bg-gray-100 text-center border border-gray-100 w-full h-full rounded-full"
-                type="text"
-                name="search"
-                placeholder="Search"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="fixed right-0 flex h-20 w-96 mr-4 items-center justify-end space-x-4">
-          <div className="h-1/2 flex items-center space-x-2 border-r px-4 ">
-            {questionMarkIcon}
-            {gearIcon}
-            {bellIcon}
-          </div>
-          <div className="flex items-center space-x-2">
-            <img
-              className="h-16 rounded-full border"
-              src="/images/avatar.png"
-            />
-            <div className="flex flex-col">
-              <p className="text-heading font-semibold">Firstname Lastname</p>
-              <p className="text-subheading">ID: 000000</p>
-            </div>
-            {angleDownIcon}
-          </div>
-        </div>
+        <BrandLogo />
+        <Search />
+        <SecondaryMenu />
       </div>
       <div className="relative w-full flex flex-col box-border">
         <div className="relative flex flex-col box-border">
@@ -160,137 +39,71 @@ export default function MainLayout(props) {
             <div className="relative flex flex-col min-h-content box-border">
               <div className="relative box-border min-h-content flex flex-row flex-nowrap items-stretch justify-center">
                 <div className="box-border w-full max-w-full flex-nowrap items-start relative flex flex-row justify-around">
-                  <div className="box-border overflow-hidden sticky w-1/6 min-h-content top-navbar-height space-y-4 py-10">
+                  <div
+                    className={`box-border overflow-hidden sticky ${
+                      slug && slug[0] && slug[1] ? "w-1/4" : "w-1/6"
+                    } min-h-content top-navbar-height space-y-4 py-10`}
+                  >
                     <Menu />
                   </div>
-                  <div className="relative w-1/2 p-10 flex flex-col border min-h-content bg-blue-50 space-y-4">
-                    <div className="w-full h-72 border relative">
-                      <img
-                        className="w-full h-full"
-                        src="/images/dashboard-content-1.svg"
-                      />
-                      <p className="absolute top-8 left-8 text-skBlue font-bold text-5xl">
-                        Hi Firstname,
-                      </p>
-                      <p className="absolute bottom-16 left-8 text-white text-2xl">
-                        Welcome Back! What will you learn today?
-                      </p>
+                  {!slug && <Home />}
+                  {slug && slug[0] != "courses" && <Home />}
+                  {/* COURSES HOME */}
+                  {slug && slug[0] == "courses" && !slug[1] && <Courses />}
+
+                  {/* COURSES - SUBJECT - LESSON OVERVIEW */}
+                  {slug && slug[0] == "courses" && slug[1] && !slug[2] && (
+                    <Course subject={slug[1]} />
+                  )}
+
+                  {/* COURSES - SUBJECT - LESSON - TOPIC VIEW */}
+                  {slug && slug[0] == "courses" && slug[1] && slug[2] && (
+                    <CourseTopic />
+                  )}
+                  {!slug && (
+                    <div className="box-border flex flex-col overflow-hidden sticky w-1/4 min-h-content top-navbar-height space-y-4 py-10">
+                      <CalendarLayout />
                     </div>
-                    <div className="w-full h-32 flex justify-around relative">
-                      <div className="h-full w-1/4 flex bg-white border rounded-2xl">
-                        <div className="w-1/3 h-full flex items-center">
-                          {pieChartIcon}
-                        </div>
-                        <div className="flex flex-col justify-center h-full w-3/4">
-                          <p className="text-skBlue font-bold text-4xl">20</p>
-                          <p className="font-semibold text-subheading">
-                            Ongoing Courses
-                          </p>
-                        </div>
-                      </div>
-                      <div className="h-full w-1/4 flex bg-white border rounded-2xl">
-                        <div className="w-1/3 h-full flex items-center">
-                          {availableCoursesIcon}
-                        </div>
-                        <div className="flex flex-col justify-center h-full w-3/4">
-                          <p className="text-skBlue font-bold text-4xl">6</p>
-                          <p className="font-semibold text-subheading">
-                            Available Courses
-                          </p>
-                        </div>
-                      </div>
-                      <div className="h-full w-1/4 flex bg-white border rounded-2xl">
-                        <div className="w-1/3 h-full flex items-center">
-                          {completedCoursesIcon}
-                        </div>
-                        <div className="flex flex-col justify-center h-full w-3/4">
-                          <p className="text-skBlue font-bold text-4xl">1</p>
-                          <p className="font-semibold text-subheading">
-                            Completed Courses
-                          </p>
-                        </div>
-                      </div>
+                  )}
+
+                  {slug && slug[0] == "courses" && !slug[1] && (
+                    <div className="box-border flex flex-col overflow-hidden sticky w-1/4 min-h-content top-navbar-height space-y-4 py-10">
+                      <CalendarLayout />
                     </div>
-                  </div>
-                  <div className="box-border flex flex-col overflow-hidden sticky w-1/4 min-h-content top-navbar-height space-y-4 py-10">
-                    <CalendarLayout />
-                  </div>
+                  )}
+                  {/* {slug && slug[0] == "courses" && slug[1] && slug[2] && (
+                    <>
+                      <div className="box-border flex flex-col overflow-hidden sticky w-1/4 min-h-content top-navbar-height space-y-4 py-10">
+                        <h1 className="text-xl px-4">Course Content</h1>
+                        <div className="box-border w-full px-4 flex flex-row justify-between">
+                          <p>Course Progress</p>
+                          <p>15%</p>
+                        </div>
+                        <div className="h-auto w-full px-4">
+                          <div className="h-6 border border-skBlue rounded-full">
+                            <div
+                              style={{ width: "15%" }}
+                              className="h-full bg-skBlue rounded-full"
+                            ></div>
+                          </div>
+                        </div>
+                        <div className="box-border w-full flex flex-col pb-4 px-4 border-b-2">
+                          <div>
+                            <h1 className="text-xl">Lesson 0: Lorem Ipsum</h1>
+                          </div>
+                          <div className="w-full flex space-x-6">
+                            <h4>1hr 50mins</h4>
+                            <h4>2/4</h4>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )} */}
                 </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <div className="flex w-full justify-around">
-          <div className="sticky w-96 border min-h-content flex flex-col space-y-4 py-10">
-            <div className="flex items-center w-full h-16 pl-10 bg-blue-50 rounded-l-full border-skBlue space-x-4 border-r-4">
-              {homeIcon}
-              <p className="text-skBlue">Home</p>
-            </div>
-            <div className="flex items-center w-full h-16 pl-10 space-x-4">
-              {bookmarkIcon}
-              <p className="text-lightGray">Courses</p>
-            </div>
-            <div className="flex items-center w-full h-16 pl-10 space-x-4">
-              {calendarIcon}
-              <p className="text-lightGray">My Schedule</p>
-            </div>
-            <div className="flex items-center w-full h-16 pl-10 space-x-4">
-              {leaderboardIcon}
-              <p className="text-lightGray">Leaderboard</p>
-            </div>
-          </div>
-          <div className="relative w-1/2 p-10 flex flex-col border min-h-content bg-blue-50 space-y-4">
-            <div className="w-full h-72 border relative">
-              <img
-                className="w-full h-full"
-                src="/images/dashboard-content-1.svg"
-              />
-              <p className="absolute top-8 left-8 text-skBlue font-bold text-5xl">
-                Hi Firstname,
-              </p>
-              <p className="absolute bottom-16 left-8 text-white text-2xl">
-                Welcome Back! What will you learn today?
-              </p>
-            </div>
-            <div className="w-full h-32 flex justify-around relative">
-              <div className="h-full w-1/4 flex bg-white border rounded-2xl">
-                <div className="w-1/3 h-full flex items-center">
-                  {pieChartIcon}
-                </div>
-                <div className="flex flex-col justify-center h-full w-3/4">
-                  <p className="text-skBlue font-bold text-4xl">20</p>
-                  <p className="font-semibold text-subheading">
-                    Ongoing Courses
-                  </p>
-                </div>
-              </div>
-              <div className="h-full w-1/4 flex bg-white border rounded-2xl">
-                <div className="w-1/3 h-full flex items-center">
-                  {availableCoursesIcon}
-                </div>
-                <div className="flex flex-col justify-center h-full w-3/4">
-                  <p className="text-skBlue font-bold text-4xl">6</p>
-                  <p className="font-semibold text-subheading">
-                    Available Courses
-                  </p>
-                </div>
-              </div>
-              <div className="h-full w-1/4 flex bg-white border rounded-2xl">
-                <div className="w-1/3 h-full flex items-center">
-                  {completedCoursesIcon}
-                </div>
-                <div className="flex flex-col justify-center h-full w-3/4">
-                  <p className="text-skBlue font-bold text-4xl">1</p>
-                  <p className="font-semibold text-subheading">
-                    Completed Courses
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="w-full h-80 border"></div>
-          </div>
-          <div className="sticky w-1/4 border min-h-content"></div>
-        </div> */}
       </div>
     </>
   );
