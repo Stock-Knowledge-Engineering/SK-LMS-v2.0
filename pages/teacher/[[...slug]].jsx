f1e8b8cb2f8e0bc9a48800b92f80960941644468
Adrian Cleofe <adriancleofe@gmail.com>
2021-07-01 16:42:56 +0800

    updated role permission

--- pages/teacher/[[...slug]].jsx
+++ pages/teacher/[[...slug]].jsx
@@ -8,9 +8,6 @@ import Start from "../../components/Start";
 
 import { useUserManagementHook } from "../../hooks/userManagementHook";
 import { useRouter } from "next/dist/client/router";
-import { socket, SocketContext } from "../../context/socket";
-import ModalLayout from "../../components/HomePage/ModalLayout";
-import AdminLayout from "../../layouts/AdminLayout";
 import FormWrapper from "../../components/HomePage/FormWrapper";
 import LMSMobileLayout from "../../layouts/LMSMobileLayout";
 import TeacherLayout from "../../layouts/TeacherLayout";
@@ -18,37 +15,9 @@ import TeacherLayout from "../../layouts/TeacherLayout";
 export default function Index(props) {
   useUserManagementHook();
 
-  const [device, setDevice] = useState('');
-  const [loginModalOpen, setLoginModalOpen] = useState(false);
-  const [onlineUserCount, setOnlineUserCount] = useState(0);
-  const [registeredUserCount, setRegisteredUserCount] = useState(0);
+  const [device, setDevice] = useState("");
   const user = useSelector((state) => state.UserReducer);
   const router = useRouter();
-
-  useEffect(() => {
-    setDevice(window.navigator.userAgent);
-
-    let data = {
-      id: 1,
-      username: "admin",
-      type: "1",
-    };
-    data ? socket.emit("CONNECT", data) : null;
-  }, []);
-
-  useEffect(() => {
-    socket.emit("ONLINE_USER_COUNT");
-
-    socket.on("ONLINE_USER_COUNT", (data) => {
-      setOnlineUserCount(data);
-    });
-
-    socket.emit("REGISTERED_USER_COUNT");
-
-    socket.on("REGISTERED_USER_COUNT", (data) => {
-      setRegisteredUserCount(data);
-    });
-  });
 
   const isMobile = () => {
     const toMatch = [
@@ -67,16 +36,19 @@ export default function Index(props) {
   };
 
   return (
-    <SocketContext.Provider value={socket}>
-      <>
-        {isMobile() && user.isLogin && user.data.title == "teacher" && (
+    <>
+      {isMobile() &&
+        user.isLogin &&
+        (user.data.title == "teacher" || user.data.title == "admin") && (
           <LMSMobileLayout />
         )}
-        {!isMobile() && user.isLogin && user.data.title == "teacher" && (
+      {!isMobile() &&
+        user.isLogin &&
+        (user.data.title == "teacher" || user.data.title == "admin") && (
           <TeacherLayout />
         )}
-        {!user.isLogin && <FormWrapper defaultForm={"login"} />}
-      </>
-    </SocketContext.Provider>
+      {!user.isLogin && <FormWrapper defaultForm={"login"} />}
+      <div id="modal-root"></div>
+    </>
   );
 }
