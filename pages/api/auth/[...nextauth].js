d2926566fc3cc6bfd3cca91531878d13cc54291e
Adrian Cleofe <rastaman8693@gmail.com>
2021-08-21 02:49:30 +0800

    remove fallback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,9 +12,6 @@ const options = {
         //     clientSecret: " "
         // })
     ],
-    pages: {
-        signIn: '/test'
-    }
 }
 
 export default (req, res) => NextAuth(req, res, options);
